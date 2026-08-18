303bd83941bead04f1cd4fe10e321d5a7bcd9a58
sdhaduk <sagardhaduk1@gmail.com>
2026-08-18 16:43:13 -0500

    added background expiration sweep

--- commander.go
+++ commander.go
@@ -6,49 +6,60 @@ import (
 )
 
 type Message struct {
-	Args []string
+	Args  []string
 	Reply chan Value
 }
 
 type entry struct {
-	val string
+	val       string
 	expiresAt time.Time
 }
 
 func RunCommander(requests chan Message) {
 	store := make(map[string]entry)
+	ticker := time.NewTicker(time.Millisecond * time.Duration(100))
+	defer ticker.Stop()
 
-	for msg := range requests {
-		switch strings.ToUpper(msg.Args[0]) {
-		case "SET":
-			msg.Reply <- cmdSet(store, msg.Args)
-			
-		case "GET":
-			msg.Reply <- cmdGet(store, msg.Args)		
-		
-		case "EXPIRE":
-			msg.Reply <- cmdExpire(store, msg.Args)
+	for {
+		select {
+		case msg, ok := <-requests:
+			if !ok {
+				return
+			}
+			switch strings.ToUpper(msg.Args[0]) {
+			case "SET":
+				msg.Reply <- cmdSet(store, msg.Args)
 
-		case "TTL":
-			msg.Reply <- cmdTTL(store, msg.Args)
+			case "GET":
+				msg.Reply <- cmdGet(store, msg.Args)
 
-		case "DEL":
-			msg.Reply <- cmdDel(store, msg.Args)
-		
-		case "EXISTS":
-			msg.Reply <- cmdExists(store, msg.Args)
-		
-		case "KEYS":
-			msg.Reply <- cmdKeys(store, msg.Args)
-			
-		case "INCR":
-			msg.Reply <- cmdIncr(store, msg.Args)
-		
-		case "DECR":
-			msg.Reply <- cmdDecr(store, msg.Args)
+			case "EXPIRE":
+				msg.Reply <- cmdExpire(store, msg.Args)
 
-		default:
-			msg.Reply <- NewError("ERR unknown command name")
+			case "TTL":
+				msg.Reply <- cmdTTL(store, msg.Args)
+
+			case "DEL":
+				msg.Reply <- cmdDel(store, msg.Args)
+
+			case "EXISTS":
+				msg.Reply <- cmdExists(store, msg.Args)
+
+			case "KEYS":
+				msg.Reply <- cmdKeys(store, msg.Args)
+
+			case "INCR":
+				msg.Reply <- cmdIncr(store, msg.Args)
+
+			case "DECR":
+				msg.Reply <- cmdDecr(store, msg.Args)
+
+			default:
+				msg.Reply <- NewError("ERR unknown command name")
+			}
+
+		case <-ticker.C:
+			sweep(store)
 		}
 	}
 }
