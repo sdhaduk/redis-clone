601f8a1add0459251ecfda33d8ebec12b7872c21
sdhaduk <sagardhaduk1@gmail.com>
2026-08-25 15:04:53 -0500

    added new commands

--- store/commander.go
+++ store/commander.go
@@ -46,6 +46,54 @@ func RunCommander(requests chan Message) {
 			case "DECR":
 				msg.Reply <- cmdDecr(store, msg.Args)
 
+			case "LPUSH":
+				msg.Reply <- cmdLPush(store, msg.Args)
+
+			case "RPUSH":
+				msg.Reply <- cmdRPush(store, msg.Args)
+
+			case "LLEN":
+				msg.Reply <- cmdLLen(store, msg.Args)
+
+			case "LRANGE":
+				msg.Reply <- cmdLRange(store, msg.Args)
+
+			case "RPOP":
+				msg.Reply <- cmdRPop(store, msg.Args)
+
+			case "LPOP":
+				msg.Reply <- cmdLPop(store, msg.Args)
+
+			case "SADD":
+				msg.Reply <- cmdSAdd(store, msg.Args)
+
+			case "SREM":
+				msg.Reply <- cmdSRem(store, msg.Args)
+
+			case "SISMEMBER":
+				msg.Reply <- cmdSIsMember(store, msg.Args)
+
+			case "SMEMBERS":
+				msg.Reply <- cmdSMembers(store, msg.Args)
+
+			case "SCARD":
+				msg.Reply <- cmdSCard(store, msg.Args)
+
+			case "ZADD":
+				msg.Reply <- cmdZAdd(store, msg.Args)
+
+			case "ZSCORE":
+				msg.Reply <- cmdZScore(store, msg.Args)
+
+			case "ZRANGE":
+				msg.Reply <- cmdZRange(store, msg.Args)
+
+			case "ZRANK":
+				msg.Reply <- cmdZRank(store, msg.Args)
+
+			case "ZREM":
+				msg.Reply <- cmdZRem(store, msg.Args)
+
 			default:
 				msg.Reply <- resp.NewError("ERR unknown command name")
 			}
